1ed108adff48e8bdd8f4dfea308e385d29aaa165
Gurhan Ozcan <gozcan83@gmail.com>
2026-02-15 21:47:18 +0300

    agent(backend-dev): issue #402 - SaaS Bootstrap Backend Expense API (#10)

---
+++ src/server/index.ts
@@ -0,0 +1,7 @@
+﻿import { createApp } from "./app";
+
+const port = Number(process.env.PORT ?? 3000);
+createApp().listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`api listening on :${port}`);
+});
